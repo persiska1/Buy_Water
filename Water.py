b4c167402363d5624998f92703f61fc5499cf71a
Persiska1 <losdavid1@gmail.com>
2022-06-06 20:17:36 +0300

    First v.0.1

---
+++ Water.py
@@ -0,0 +1,29 @@
+from tkinter import W
+
+
+vater = 100
+money = 0
+def buy_water():
+    guy_money = int(input('внесите деньги 5,10,50,100:'))
+    if guy_money >= 100:
+        print(input('Сколько литров?' ))
+        
+        if 25:
+            print(3)
+            print(2)
+            print(1)
+
+            all_vater = vater - 25
+            print('Заберите воду')
+
+    else: 
+        return 
+buy_water()
+
+print('\n') 
+
+
+
+
+        
+
